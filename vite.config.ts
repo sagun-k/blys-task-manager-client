51b0af96277f0899d34edfa2863fe197ab302382
sagun-k <sagunkshrestha7@gmail.com>
2025-07-25 01:17:31 +0545

    removed server proxy

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   base:'/',
-  plugins: [react()],
-  server: {
-    proxy: process.env.NODE_ENV === 'development' ? {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        secure: false,
-      },
-    } : undefined,
-  },
+  plugins: [react()]
 });
