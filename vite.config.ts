c4d6a87b0875d0bcc0e61677526226148caa53e2
sagun-k <sagunkshrestha7@gmail.com>
2025-07-25 01:13:34 +0545

    Updated the vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base:'/',
   plugins: [react()],
   server: {
     proxy: process.env.NODE_ENV === 'development' ? {
